f842ebaa3febcd74a9b10c450767acd7c2b6352e
sravanth3108 <sravanthreddy.pullamgari@gmail.com>
2022-11-30 17:12:56 -0600

    Competed bonus for assignment 13

--- app.js
+++ app.js
@@ -72,8 +72,7 @@ app.use('/resource', resourceRouter);
 // passport config 
 // Use the existing connection 
 // The Account model  
-var Account =require('./models/account'); 
- 
+var Account =require('./models/account');  
 passport.use(new LocalStrategy(Account.authenticate())); 
 passport.serializeUser(Account.serializeUser()); 
 passport.deserializeUser(Account.deserializeUser()); 
--- routes/curriculum.js
+++ routes/curriculum.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const curriculum_controlers= require('../controllers/curriculum');
 var router = express.Router();
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET curriculums */
 router.get('/', curriculum_controlers.curriculum_view_all_Page );
 /* GET detail curriculum page */ 
@@ -8,7 +15,8 @@ router.get('/detail/', curriculum_controlers.curriculum_view_one_Page);
 /* GET create curriculum page */ 
 router.get('/create', curriculum_controlers.curriculum_create_Page); 
 /* GET create update page */ 
-router.get('/update', curriculum_controlers.curriculum_update_Page); 
+router.get('/update',secured ,curriculum_controlers.curriculum_update_Page); 
 /* GET delete curriculum page */ 
 router.get('/delete', curriculum_controlers.curriculum_delete_Page); 
+
 module.exports = router;
